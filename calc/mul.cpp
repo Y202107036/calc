7e6d180d902bae1d4ecb5663ab98af3ef2b59e9a
Y202107036 <bdod19@gmail.com>
2025-06-05 19:03:54 +0900

    dev/mul branch [ + ] 곱셈 연산 추가

--- calc/mul.cpp
+++ calc/mul.cpp
@@ -1,1 +1,17 @@
- 
+#include "add.h"
+
+int mul(int x, int y)
+{
+	int i = 0;           /// 반복문용 변수
+	int sum = 0;         /// 누적 값 보관 변수
+	
+        /// 3 * 5 = 3 + 3 + 3 + 3 + 3
+	for(i=0; i<y; i++)
+	{
+		sum = add(x, sum);
+		/// 3, 0 => 3 = sum
+		/// 3, 3 => 6
+		/// 3, 6 => 9
+	}
+	return sum;
+}
